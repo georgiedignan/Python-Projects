b7458447ba4f9911100adb02e5905d70029f37a9
Georgie Dignan <georgiedignan@gmail.com>
2021-08-09 13:26:11 +1000

    Moved files into git repo

---
+++ parity_project/starter/parity.py
@@ -0,0 +1,188 @@
+import csv
+
+
+def load_grid(csv_file_path):
+    """Loads data from a csv file.
+
+    Args:
+        csv_file_path: string representing the path to a csv file.
+    Returns:
+        A list of lists, where each sublist is a line in the csv file.
+    """
+    data = []
+
+    with open(csv_file_path, mode = 'r', encoding = "utf-8") as csv_file:
+        csv_reader = csv.reader(csv_file,delimiter=",")
+        for line in csv_reader:
+            data.append(line)
+    return data
+
+
+# print(load_grid('tests/grids/medium_grid.csv'))
+
+def add_column(grid):
+    """Adds a new column to a grid. For each row, if there is an even
+    number of X characters, a O is added to the row, otherwise a X is added
+    to the row.
+
+    Arguments:
+        grid: A list of lists, where each sublist represents a row in a grid.
+
+    Returns:
+        The same grid, with a new column added.
+    """
+    if grid == []:
+        return grid
+
+    new_column = []
+    for row in grid:
+        # print(row)
+        count_x = 0
+        for char in row:
+        #count the number of x's 
+            if char == 'X':
+                count_x += 1
+        if count_x%2 == 0:
+            row.append('O')
+        else:
+            row.append('X')
+
+        # check the new column
+        # print(count_x)
+        # print(row[-1])
+    # print(grid)
+    return grid
+    
+# grid = load_grid('tests/grids/medium_grid.csv')
+# x = add_column(grid)
+# print(x)
+
+def add_row(grid):
+    """Adds a new row to a grid. For each column, if there is an even
+    number of X characters, a O is added to the column, otherwise a X is added
+    to the column.
+
+    Arguments:
+        grid: A list of lists, where each sublist represents a row in a grid.
+
+    Returns:
+        The same grid, with a new row added.
+    """
+    new_line = []
+    if grid == []:
+        return grid
+    else:
+        for i in range(len(grid[0])):
+            count_x = 0
+            for row in grid:
+                if row[i] == "X":
+                    count_x += 1
+            if count_x % 2 == 0:
+                new_line.append('O')
+            else:
+                new_line.append('X')
+    grid.append(new_line)
+    return grid
+
+# grid = load_grid('tests/grids/medium_grid.csv')
+# print(add_row(grid))
+
+
+def flip_cell(x_pos, y_pos, grid):
+    """Prompts the user to choose a cell to swap from X to O (or vice versa).
+
+    Arguments:
+        x_pos: An integer representing the x coordinate of the cell.
+        y_pos: An integer representing the y coordinate of the cell.
+        grid: A list of lists, where each sublist represents a row in a grid.
+
+        In the following grid:
+            a b
+            c d
+        These are the coordinates of each letter:
+            a = x: 0, y: 0
+            b = x: 1, y: 0
+            c = x: 0, y: 1
+            d = x: 1, y: 1
+
+    Returns:
+        The same grid, with a cell switched.
+    """
+
+    #CURRENT ANSWER
+
+    #check current grid position
+    # print(grid[y_pos][x_pos])
+
+    #flip the cell
+    if grid == []:
+        return grid
+    else:
+        if grid[y_pos][x_pos] == 'X':
+            #is this the correct way to reassign a position ??
+            grid[y_pos][x_pos] = 'O'
+        else:
+            grid[y_pos][x_pos] = 'X'
+
+    #check if the cell is flipped
+    # print(grid[y_pos][x_pos])
+
+    return grid 
+
+# grid = load_grid('tests/grids/medium_grid.csv')
+# flip_cell = flip_cell(0,1,grid)
+# print(flip_cell)
+
+
+def find_flipped_cell(grid):
+    """Determines which cell/cell in the grid was flipped.
+
+    Arguments:
+        grid: A list of lists, where each sublist represents a row in a grid.
+
+    Returns:
+        A list containing the coordinates of the cell with the flipped cell.
+        In the following grid:
+            a b
+            c d
+        These are the coordinates of each letter:
+            a = (0, 0)
+            b = (1, 0)
+            c = (0, 1)
+            d = (1, 1)
+        If 'a' was the flipped letter, this function would return: [0, 0]
+    """
+    #LOGIC
+    #find the column and row with the odd number of x's
+
+    coords = []
+
+    xsum = sum(x.count("X") for x in grid)
+    if xsum %2 == 0:
+        coords.append(None)
+        coords.append(None)
+
+    #X_position
+    for column in range(len(grid)):
+        x_count = 0
+        for row in grid:
+            if row[column] == 'X':
+                x_count += 1
+        if x_count%2 != 0:
+            coords.append(column)
+
+    #Y_position
+    for y,row in enumerate(grid):
+        x_count = 0
+        for char in row:
+            if char == "X":
+                x_count += 1
+        if x_count%2 != 0:
+            coords.append(y)
+    
+    return coords
+
+# test
+# grid = load_grid('tests/grids/medium_grid_flipped.csv')
+# fliped_cell = find_flipped_cell(grid)
+# print(fliped_cell)
